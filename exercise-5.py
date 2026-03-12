5eba6a1b471ebf58b2642ad8232785ccb36935ac
Kelia Iradukunda <iradukundakelia@gmail.com>
2026-03-12 19:16:15 +0100

    Define a repeat function

--- exercise-5.py
+++ exercise-5.py
@@ -4,3 +4,10 @@
 #Create a function called repeat. 
 # It takes two parameters, a string and a count, and returns a new string that is the old one repeated count times.
 # ============================================================================
+
+def repeat(str1, counter):
+    repeated_string = ''
+    for i in range(0, counter - 1):
+        repeated_string += str1
+
+    return repeated_string
