0bde76c3ed1100fd8f1ffefebde4e2cd52f14557
Kelia Iradukunda <iradukundakelia@gmail.com>
2026-03-13 18:51:11 +0200

    Use conditional statements to give output

--- exercise-8.py
+++ exercise-8.py
@@ -44,6 +44,24 @@
 #       → Print True if the person has a car OR has a ride-share app
 #       → Otherwise, print False
 #
+
+distance_mi = 1
+is_raining = True
+has_bike = True
+has_car = True
+has_ride_share_app = True
+
+if not bool(distance_mi):
+    print(False)
+elif distance_mi <= 1 and is_raining == False:
+    print(True)
+elif distance_mi in range(2, 7) and has_bike == True and is_raining == False:
+    print(True)
+elif distance_mi > 6 and has_car == True or has_ride_share_app == True:
+    print(True)
+else:
+    print(False)
+
 # ===========================================================================
 # TESTS TO PASS:
 # ===========================================================================
